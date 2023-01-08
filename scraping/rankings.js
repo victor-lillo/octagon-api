c6fee8ab514498dba4d83a7bba067a26eaa4735f
fentosv <fentos.dev@gmail.com>
2023-01-08 22:42:51 +0100

    Add ranking scraper

--- scraping/rankings.js
+++ scraping/rankings.js
@@ -1,18 +1,50 @@
-import * as cheerio from 'cheerio'
+import { parse } from 'node-html-parser'
+import { writeDBFile } from './lib/db.js'
+
+// http://www.ufcstats.com/statistics/fighters?char=a
 
 const RANKINGS_URL = 'https://www.ufc.com/rankings'
+
+const SELECTORS = {
+  table: '.views-table',
+  category: '.rankings--athlete--champion h4',
+  champion: '.rankings--athlete--champion .views-row a',
+  rankeds: '.views-row',
+}
+
+const cleanString = text => {
+  return text.replace(/\t|\n|\s:/g, '').trim()
+}
 
 async function getRankings() {
   const res = await fetch(RANKINGS_URL)
   const html = await res.text()
-  const $ = cheerio.load(html)
+  const $ = parse(html)
 
-  const $cards = $('.a-carousel-card')
+  const $tables = $.querySelectorAll(SELECTORS.table)
 
-  console.log($cards.length)
-  //   $cards.each((index, el) => {
-  //     const $el = $(el)
-  //   })
+  const data = []
+
+  // Recorremos el nodeElement tables (que son todos los que coinciden con nuestra búsqueda)
+  $tables.forEach($el => {
+    const obj = {}
+
+    const category = $el.querySelector(SELECTORS.category).textContent
+    const champion = $el.querySelector(SELECTORS.champion)
+      ? $el.querySelector(SELECTORS.champion).textContent
+      : ''
+
+    const rankedsNodeElements = $el.querySelectorAll(SELECTORS.rankeds)
+    const rankeds = rankedsNodeElements.map(element => cleanString(element.textContent))
+
+    obj.category = category
+    obj.champion = champion
+    obj.fighters = rankeds
+
+    data.push(obj)
+  })
+
+  await writeDBFile('rankings', data)
 }
 
 const run = async () => {
