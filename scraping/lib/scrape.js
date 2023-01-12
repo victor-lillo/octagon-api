1d6bc561b7152a73647b5b0c0dad03f7b814661c
fentosv <fentos.dev@gmail.com>
2023-01-12 13:03:03 +0100

    Swipe requests from fetch to axios

--- scraping/lib/scrape.js
+++ scraping/lib/scrape.js
@@ -1,9 +1,20 @@
+import axios from 'axios'
 import { parse } from 'node-html-parser'
 
 const scrape = async (url) => {
-  const res = await fetch(url)
-  const html = await res.text()
-  return parse(html)
+  try {
+    // Usamos Axios porque fetch (node v 18.13) falla con 'fetch failed' al hacer un get a un dominio con response 301, no hace el redirect automáticamente.
+    const res = await axios.get(url)
+    const html = await res.data
+    return parse(html)
+    /*
+    Con fetch sería así:
+      const res = await fetch(url)
+      const html = await res.text()
+    */
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 
 export default scrape
