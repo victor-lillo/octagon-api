0d03b8c7cf338ee047dbc2fc036fa88bae1339e7
fentosv <fentos.dev@gmail.com>
2023-01-13 16:58:51 +0100

    Improve timeFormatter

--- scraping/lib/timeFormatter.js
+++ scraping/lib/timeFormatter.js
@@ -1,7 +1,30 @@
-// Time: ms
+const DISPLAY_UNITS = {
+  milliseconds: 'ms',
+  seconds: 'sec',
+  minutes: 'min',
+  hours: 'h',
+}
+const padNumber = (num) => {
+  return num.toString().padStart(2, '0')
+}
+
+/**
+ * @param time {number} (in milliseconds)
+ */
 const timeFormatter = (time) => {
-  if (time > 1000) return (time / 1000).toFixed(2)
-  return time.toFixed(2)
+  const parsedTime = new Date(time)
+
+  if (time > 1000 * 60 * 60)
+    return `${parsedTime.getHours()}:${padNumber(parsedTime.getMinutes())}:${padNumber(
+      parsedTime.getSeconds()
+    )} ${DISPLAY_UNITS.hours}`
+  else if (time > 1000 * 60)
+    return `${parsedTime.getMinutes()}:${padNumber(parsedTime.getSeconds())} ${
+      DISPLAY_UNITS.minutes
+    }`
+  else if (time > 1000)
+    return `${parsedTime.getSeconds()}.${parsedTime.getMilliseconds()} ${DISPLAY_UNITS.seconds}`
+  else return `${time.toFixed(2)} ${DISPLAY_UNITS.milliseconds}`
 }
 
 export default timeFormatter
