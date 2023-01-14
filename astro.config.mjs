bd423f53a78c9ebaa4f47cd935c4cc86891cbeac
fentosv <fentos.dev@gmail.com>
2023-01-14 13:23:12 +0100

    Install @astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+import image from '@astrojs/image'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image()],
+})
