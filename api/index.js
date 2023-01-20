194a6ee299531625d77d849e1b694c4344e4030d
fentosv <fentos.dev@gmail.com>
2023-01-20 12:16:02 +0100

    Rename project and easier title setting up

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import fighters from '@db/fighters.json'
 import rankings from '@db/rankings.json'
 
 const app = new Hono()
-app.get('/', (context) => context.text('Welcome to the unofficial UFC API'))
+app.get('/', (context) => context.text('Welcome to Octogon API'))
 app.get('/fighters', (context) => context.json(fighters))
 app.get('/rankings', (context) => context.json(rankings))
 app.get('/fighter/:fighterId', (context) => {
