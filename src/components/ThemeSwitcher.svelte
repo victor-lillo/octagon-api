8e12f4d1747cc5b01be9edd9bd807272a5d0eb18
fentosv <fentos.dev@gmail.com>
2023-01-28 13:45:47 +0100

    Add ThemeSwitcher component

---
+++ src/components/ThemeSwitcher.svelte
@@ -0,0 +1,30 @@
+<script>
+  const DARK_THEME_CLASS = 'theme-dark'
+
+  let isDark = false
+
+  function handleClick() {
+    console.log(isDark)
+    isDark = !isDark
+
+    const root = document.documentElement
+    root.classList.toggle(DARK_THEME_CLASS, isDark)
+  }
+</script>
+
+<button on:click={handleClick}>
+  {isDark ? '🌞' : '🌑'}
+</button>
+
+<style>
+  button {
+    background: none;
+    border: 1px solid transparent;
+    font-size: 1.2rem;
+    padding: 0.6rem;
+  }
+
+  button:hover {
+    border-color: var(--color-primary-light-3);
+  }
+</style>
