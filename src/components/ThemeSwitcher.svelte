07a4be86bd123026347059191f701e1468d83e85
fentosv <fentos.dev@gmail.com>
2023-01-28 15:41:15 +0100

    Add localStorage management

--- src/components/ThemeSwitcher.svelte
+++ src/components/ThemeSwitcher.svelte
@@ -1,13 +1,20 @@
 <script>
+  const STORAGE_THEME_KEY = 'theme'
   const DARK_THEME_CLASS = 'theme-dark'
 
   let isDark = false
 
   function handleClick() {
     isDark = !isDark
-
     const root = document.documentElement
     root.classList.toggle(DARK_THEME_CLASS, isDark)
+    localStorage.setItem(STORAGE_THEME_KEY, isDark ? 'dark' : 'light')
+  }
+
+  $: {
+    if (!import.meta.env.SSR) {
+      isDark = document.documentElement.classList.contains('theme-dark')
+    }
   }
 </script>
 
