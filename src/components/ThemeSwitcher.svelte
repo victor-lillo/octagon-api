dd5f9a26b296d683d5af6e5af3f1b1177918d603
fentosv <fentos.dev@gmail.com>
2023-02-06 18:40:37 +0100

    Tweak ThemeSwitcher icon size

--- src/components/ThemeSwitcher.svelte
+++ src/components/ThemeSwitcher.svelte
@@ -1,19 +1,19 @@
 <script>
-  const STORAGE_THEME_KEY = 'theme'
-  const DARK_THEME_CLASS = 'theme-dark'
+  const STORAGE_THEME_KEY = 'theme';
+  const DARK_THEME_CLASS = 'theme-dark';
 
-  let isDark = false
+  let isDark = false;
 
   function handleClick() {
-    isDark = !isDark
-    const root = document.documentElement
-    root.classList.toggle(DARK_THEME_CLASS, isDark)
-    localStorage.setItem(STORAGE_THEME_KEY, isDark ? 'dark' : 'light')
+    isDark = !isDark;
+    const root = document.documentElement;
+    root.classList.toggle(DARK_THEME_CLASS, isDark);
+    localStorage.setItem(STORAGE_THEME_KEY, isDark ? 'dark' : 'light');
   }
 
   $: {
     if (!import.meta.env.SSR) {
-      isDark = document.documentElement.classList.contains('theme-dark')
+      isDark = document.documentElement.classList.contains('theme-dark');
     }
   }
 </script>
@@ -26,7 +26,7 @@
   button {
     background: none;
     border: 1px solid transparent;
-    font-size: 1.2rem;
+    font-size: 1.7rem;
     padding: 0.6rem;
   }
 
