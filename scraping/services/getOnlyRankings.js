e79593ebd04e50f15bb8520eb59010a8083be0dd
fentosv <fentos.dev@gmail.com>
2023-02-07 17:45:07 +0100

    Implement comparing feature

--- scraping/services/getOnlyRankings.js
+++ scraping/services/getOnlyRankings.js
@@ -1,15 +1,17 @@
+import { compareRankings } from '../lib/compareRankings.js'
 import { logSuccess } from '../lib/log.js'
-import timeFormatter from '../lib/timeFormatter.js'
 import scrapeRankingsInfo from '../lib/scrapeRankingsInfo.js'
+import timeFormatter from '../lib/timeFormatter.js'
 
 const run = async () => {
   const start = performance.now()
 
   await scrapeRankingsInfo()
+  await compareRankings()
 
   const end = performance.now()
   const time = timeFormatter(end - start)
-  logSuccess(`Task finished in ${time}`)
+  logSuccess(`\nTask finished in ${time}`)
 }
 
 run()
