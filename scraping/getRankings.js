4e47f3ff877589ada2c3e337b49ba6a70496e2ce
fentosv <fentos.dev@gmail.com>
2023-02-16 10:22:07 +0100

    Add compareRankings to getRankings

--- scraping/getRankings.js
+++ scraping/getRankings.js
@@ -1,7 +1,9 @@
+import { compareRankings } from './lib/compareRankings.js'
 import scrapeRankingInfo from './lib/scrapeRankingsInfo.js'
 
 const getRankings = async () => {
   await scrapeRankingInfo()
+  await compareRankings()
 }
 
 export default getRankings
