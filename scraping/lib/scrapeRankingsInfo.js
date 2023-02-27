85f0caeb77099e1d0fd4b0a7664f4b8092736471
fentosv <fentos.dev@gmail.com>
2023-02-27 18:50:04 +0100

    Error handling: not fighters found in scrape

--- scraping/lib/scrapeRankingsInfo.js
+++ scraping/lib/scrapeRankingsInfo.js
@@ -1,5 +1,5 @@
 import { cleanString, standarizeString } from './string_modifiers.js'
-import { logInfo, logSuccess } from './log.js'
+import { logError, logInfo, logSuccess } from './log.js'
 import { writeDBFile } from './db.js'
 import scrape from './scrape.js'
 import { renameFile } from './renameFile.js'
@@ -20,38 +20,48 @@ const getSlugFromUrl = (url) => {
 }
 
 async function scrapeRankingsInfo() {
-  logInfo(`Running ranking scraper`)
+  try {
+    logInfo(`Running ranking scraper`)
 
-  const $ = await scrape(RANKINGS_URL)
-  const $tables = $.querySelectorAll(SELECTORS.table)
+    const $ = await scrape(RANKINGS_URL)
+    const $tables = $.querySelectorAll(SELECTORS.table)
 
-  // Recorremos el nodeElement tables (que son todos los que coinciden con nuestra búsqueda)
-  const data = $tables.map(($el) => {
-    const categoryName = $el.querySelector(SELECTORS.category).textContent
-    const categoryId = standarizeString(categoryName)
-    const championNode = $el.querySelector(SELECTORS.champion)
-      ? $el.querySelector(SELECTORS.champion)
-      : ''
-    const championName = championNode ? championNode.textContent : ''
-    const championId = championNode ? getSlugFromUrl(championNode.getAttribute('href')) : ''
+    // Recorremos el nodeElement tables (que son todos los que coinciden con nuestra búsqueda)
+    const data = $tables.map(($el) => {
+      const categoryName = $el.querySelector(SELECTORS.category).textContent
+      const categoryId = standarizeString(categoryName)
+      const championNode = $el.querySelector(SELECTORS.champion)
+        ? $el.querySelector(SELECTORS.champion)
+        : ''
+      const championName = championNode ? championNode.textContent : ''
+      const championId = championNode ? getSlugFromUrl(championNode.getAttribute('href')) : ''
 
-    const rankedsNodeElements = $el.querySelectorAll(SELECTORS.fighterRows)
+      const rankedsNodeElements = $el.querySelectorAll(SELECTORS.fighterRows)
 
-    const fighters = rankedsNodeElements.map(($element) => {
-      const relativeFighterUrl = $element.querySelector('a').getAttribute('href')
-      const id = getSlugFromUrl(relativeFighterUrl)
-      const name = cleanString($element.textContent)
-      return { id, name }
+      const fighters = rankedsNodeElements.map(($element) => {
+        const relativeFighterUrl = $element.querySelector('a').getAttribute('href')
+        const id = getSlugFromUrl(relativeFighterUrl)
+        const name = cleanString($element.textContent)
+        return { id, name }
+      })
+
+      const champion = { id: championId, championName }
+
+      // Guard clause: if fighters is 0, throw new Error
+      if (fighters.length === 0)
+        throw new Error(`Fighters not found. Check the selectors in ${RANKINGS_URL}`)
+
+      return { id: categoryId, categoryName, champion, fighters }
     })
 
-    const champion = { id: championId, championName }
-
-    return { id: categoryId, categoryName, champion, fighters }
-  })
-
-  await renameFile('rankings', 'rankings-old')
-  await writeDBFile(RANKINGS_DB_NAME, data)
-  logSuccess(`Rankings saved in ${RANKINGS_DB_NAME}.json\n`)
+    await renameFile('rankings', 'rankings-old')
+    await writeDBFile(RANKINGS_DB_NAME, data)
+    logSuccess(`Rankings saved in ${RANKINGS_DB_NAME}.json\n`)
+  } catch (error) {
+    console.log('')
+    logError(error)
+    console.log('')
+  }
 }
 
 export default scrapeRankingsInfo
