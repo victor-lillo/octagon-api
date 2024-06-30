304f592fd1aa69a24c23dddf59dbcd30fc01d42d
fentosv <fentos.dev@gmail.com>
2024-06-30 18:06:20 +0200

    Move renameFile to db & rename to renameDBFile

--- scraping/utils/db.js
+++ scraping/utils/db.js
@@ -1,5 +1,6 @@
 import { join } from 'node:path'
 import { readFile, writeFile } from 'node:fs/promises'
+import { rename } from 'fs'
 import { DB_FOLDER_NAME } from '../constants/index.js'
 
 export const getDBFilePath = (dbName) => join(process.cwd(), DB_FOLDER_NAME, dbName)
@@ -15,4 +16,16 @@ export const writeDBFile = async (dbName, data) => {
   const filePath = getDBFilePath(dbName)
 
   return writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8')
+}
+
+export const renameDBFile = async (oldFileName, newFileName) => {
+  rename(
+    getDBFilePath(oldFileName),
+    getDBFilePath(newFileName),
+
+    (err) => {
+      if (err) throw err
+      console.log(`"${oldFileName}" saved in "${newFileName}"\n`)
+    }
+  )
 }
