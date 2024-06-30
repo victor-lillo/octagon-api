b35cb8c5a54d92b447742ed1aedf0da1154f1bd5
fentosv <fentos.dev@gmail.com>
2024-06-30 15:18:45 +0200

    Update getRanking with the new SRP scrapeRankings

--- scraping/getRankings.js
+++ scraping/getRankings.js
@@ -1,8 +1,18 @@
 import { compareRankings } from './services/compareRankings.js'
 import scrapeRanking from './scrapers/scrapeRankings.js'
+import { renameFile } from './utils/renameFile.js'
+import { writeDBFile } from './utils/db.js'
+import { logSuccess } from './utils/log.js'
+
+const RANKINGS_DB_NAME = 'rankings'
 
 const getRankings = async () => {
-  await scrapeRanking()
+  const data = await scrapeRanking()
+  await renameFile('rankings', 'rankings-old')
+  await writeDBFile(RANKINGS_DB_NAME, data)
+
+  logSuccess(`Rankings saved in ${RANKINGS_DB_NAME}.json\n`)
+
   await compareRankings()
 }
 
