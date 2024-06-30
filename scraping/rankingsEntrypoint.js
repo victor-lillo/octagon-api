df80641064ef29c0981aa55d5515835262e445de
fentosv <fentos.dev@gmail.com>
2024-06-30 20:47:38 +0200

    Update rankingsEntrypoint

--- scraping/rankingsEntrypoint.js
+++ scraping/rankingsEntrypoint.js
@@ -1,17 +1,8 @@
-import { compareRankings } from './services/compareRankings.js'
-import { logSuccess } from './utils/log.js'
-import { renameDBFile, writeDBFile } from './utils/db.js'
-import scrapeRankings from './scrapers/scrapeRankings.js'
-import { RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME } from './constants/names.js'
+import { getAndSaveRankings } from './services/getAndSaveRankings.js'
 
-const getRankings = async () => {
-  const data = await scrapeRankings()
-  await renameDBFile(RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME)
-  await writeDBFile(RANKINGS_DB_NAME, data)
-
-  logSuccess(`Rankings saved in ${RANKINGS_DB_NAME}\n`)
-
-  await compareRankings()
+const runRankingsEntrypoint = async () => {
+  console.log('Starting scraping process from runRankingsEntrypoint...')
+  await getAndSaveRankings()
 }
 
-export default getRankings
+export default runRankingsEntrypoint
