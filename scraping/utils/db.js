ac55c5f1b3759fc911894a97385d2dc197aae8d7
fentosv <fentos.dev@gmail.com>
2024-06-30 16:57:49 +0200

    Implement constant & convert to arrow functions

--- scraping/utils/db.js
+++ scraping/utils/db.js
@@ -1,19 +1,18 @@
 import { join } from 'node:path'
 import { readFile, writeFile } from 'node:fs/promises'
+import { DB_FOLDER_NAME } from '../constants/index.js'
 
-const DB_FOLDER_NAME = 'db'
+export const getDBFilePath = (dbName) => join(process.cwd(), DB_FOLDER_NAME, dbName)
 
-const getFilePath = (dbName) => join(process.cwd(), DB_FOLDER_NAME, dbName)
-
-export async function readDBFile(dbName) {
-  const filePath = getFilePath(dbName)
+export const readDBFile = async (dbName) => {
+  const filePath = getDBFilePath(dbName)
 
   const text = await readFile(filePath, 'utf-8')
   return JSON.parse(text)
 }
 
-export function writeDBFile(dbName, data) {
-  const filePath = getFilePath(dbName)
+export const writeDBFile = async (dbName, data) => {
+  const filePath = getDBFilePath(dbName)
 
   return writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8')
 }
