626b21b6008fe42499dc50b4dcd195a8aee7058a
fentosv <fentos.dev@gmail.com>
2024-06-30 15:47:29 +0200

    Update getOnlyRankings.js

--- scraping/services/getOnlyRankings.js
+++ scraping/services/getOnlyRankings.js
@@ -1,12 +1,17 @@
 import { compareRankings } from './compareRankings.js'
 import { logSuccess } from '../utils/log.js'
+import { renameFile } from '../utils/renameFile.js'
+import { writeDBFile } from '../utils/db.js'
 import scrapeRankings from '../scrapers/scrapeRankings.js'
 import timeFormatter from '../utils/timeFormatter.js'
 
 const run = async () => {
   const start = performance.now()
 
-  await scrapeRankings()
+  const data = await scrapeRankings()
+  await renameFile('rankings', 'rankings-old')
+  await writeDBFile(RANKINGS_DB_NAME, data)
+
   await compareRankings()
 
   const end = performance.now()
