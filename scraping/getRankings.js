e93d874f2c06ae8406514c1d1e3d0a2ac712f8a5
fentosv <fentos.dev@gmail.com>
2024-06-30 15:30:55 +0200

    Update default function import name

--- scraping/getRankings.js
+++ scraping/getRankings.js
@@ -1,5 +1,5 @@
 import { compareRankings } from './services/compareRankings.js'
-import scrapeRanking from './scrapers/scrapeRankings.js'
+import scrapeRankings from './scrapers/scrapeRankings.js'
 import { renameFile } from './utils/renameFile.js'
 import { writeDBFile } from './utils/db.js'
 import { logSuccess } from './utils/log.js'
@@ -7,7 +7,7 @@ import { logSuccess } from './utils/log.js'
 const RANKINGS_DB_NAME = 'rankings'
 
 const getRankings = async () => {
-  const data = await scrapeRanking()
+  const data = await scrapeRankings()
   await renameFile('rankings', 'rankings-old')
   await writeDBFile(RANKINGS_DB_NAME, data)
 
