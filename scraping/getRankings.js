6dfddb5a6d6bfe76e58c4d644289d20277a5eee5
fentosv <fentos.dev@gmail.com>
2024-06-30 18:26:57 +0200

    Implement renameDBFile import changes

--- scraping/getRankings.js
+++ scraping/getRankings.js
@@ -1,13 +1,12 @@
 import { compareRankings } from './services/compareRankings.js'
 import { logSuccess } from './utils/log.js'
-import { renameFile } from './utils/renameFile.js'
-import { writeDBFile } from './utils/db.js'
+import { renameDBFile, writeDBFile } from './utils/db.js'
 import scrapeRankings from './scrapers/scrapeRankings.js'
 import { RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME } from './constants/index.js'
 
 const getRankings = async () => {
   const data = await scrapeRankings()
-  await renameFile(RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME)
+  await renameDBFile(RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME)
   await writeDBFile(RANKINGS_DB_NAME, data)
 
   logSuccess(`Rankings saved in ${RANKINGS_DB_NAME}\n`)
--- scraping/services/getOnlyRankings.js
+++ scraping/services/getOnlyRankings.js
@@ -1,7 +1,6 @@
 import { compareRankings } from './compareRankings.js'
 import { logSuccess } from '../utils/log.js'
-import { renameFile } from '../utils/renameFile.js'
-import { writeDBFile } from '../utils/db.js'
+import { renameDBFile, writeDBFile } from '../utils/db.js'
 import scrapeRankings from '../scrapers/scrapeRankings.js'
 import timeFormatter from '../utils/timeFormatter.js'
 import { RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME } from '../constants/index.js'
@@ -10,7 +9,7 @@ const run = async () => {
   const start = performance.now()
 
   const data = await scrapeRankings()
-  await renameFile(RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME)
+  await renameDBFile(RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME)
   await writeDBFile(RANKINGS_DB_NAME, data)
 
   await compareRankings()
