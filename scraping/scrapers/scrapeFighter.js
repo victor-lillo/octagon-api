0436192a792b907956074a3f8a94d6c2b16f2de2
fentosv <fentos.dev@gmail.com>
2024-07-01 11:49:46 +0200

    Apply SRP in scrapeFighter.js

--- scraping/scrapers/scrapeFighter.js
+++ scraping/scrapers/scrapeFighter.js
@@ -1,17 +1,10 @@
-import path from 'node:path'
-import { cleanString, camelCaseString } from '../utils/stringFormatters.js'
-import { logInfo, logSuccess } from '../utils/log.js'
-import { readDBFile, writeDBFile } from '../utils/db.js'
-import checkFileExists from '../utils/checkFileExists.js'
-import saveImage from '../utils/saveImage.js'
+import { camelCaseString, cleanString } from '../utils/stringFormatters.js'
+import { logInfo } from '../utils/log.js'
 import scrape from '../utils/scrape.js'
-import { FIGHTERS_DB_NAME } from '../constants/names.js'
 
 const ATHLETE_BASE_URL = 'https://www.ufc.com/athlete/'
-const BASE_IMAGE_FOLDER = 'public'
-const FIGHTERS_IMAGE_FOLDER = 'fighters'
 
-const FIGHTER_DATA = await readDBFile(FIGHTERS_DB_NAME)
+const getAthleteUrl = (id) => ATHLETE_BASE_URL + id
 
 const SELECTORS = {
   bio_field: '.c-bio__label',
@@ -23,9 +16,9 @@ const SELECTORS = {
 }
 
 const scrapeFighter = async (id) => {
-  const url = ATHLETE_BASE_URL + id
-
   logInfo(`Scraping ${id} web page`)
+
+  const url = getAthleteUrl(id)
 
   const $ = await scrape(url)
 
@@ -38,30 +31,17 @@ const scrapeFighter = async (id) => {
   const [wins, losses, draws] = rawRecord.split(' ')[0].split('-')
   const category = $.querySelector(SELECTORS.category).textContent
 
-  const bioFieldsData = {}
   const $bioFields = $.querySelectorAll(SELECTORS.bio_field)
 
+  const bioFieldsData = {}
   $bioFields.forEach(($el) => {
     const text_label = $el.textContent.toLowerCase().replace(' ', '_')
     const text_content = cleanString($el.nextElementSibling.textContent)
     bioFieldsData[camelCaseString(text_label)] = text_content
   })
 
-  // For new fighters in our ranking, FIGHTER_DATA[id] is undef
-  // So we return a void string as imgUrl
-  const oldImgUrl = FIGHTER_DATA[id]?.imgUrl ?? ''
-
-  if (imgUrl !== oldImgUrl)
-    await saveImage({
-      baseFolder: BASE_IMAGE_FOLDER,
-      fileName: id,
-      folder: FIGHTERS_IMAGE_FOLDER,
-      url: imgUrl,
-    })
-
-  FIGHTER_DATA[id] = { category, draws, imgUrl, losses, name, nickname, wins, ...bioFieldsData }
-  await writeDBFile(FIGHTERS_DB_NAME, FIGHTER_DATA)
-  logSuccess(`${FIGHTERS_DB_NAME} updated`)
+  const data = { category, draws, imgUrl, losses, name, nickname, wins, ...bioFieldsData }
+  return data
 }
 
 export default scrapeFighter
