44eb530b203852c6a41175ac48885e66cb116678
fentosv <fentos.dev@gmail.com>
2024-07-01 09:38:38 +0200

    Add compareAndSaveRankings.js

---
+++ scraping/services/compareAndSaveRankings.js
@@ -0,0 +1,13 @@
+import { getAndSaveRankings } from './getAndSaveRankings.js'
+import { logSuccess } from '../utils/log.js'
+import { writeDBFile } from '../utils/db.js'
+import { NEWS_DB_NAME } from '../constants/names.js'
+import { STORED_CHANGES_NUM } from '../constants/config.js'
+
+export const compareRankings = async () => {
+  const rankingChanges = await getAndSaveRankings()
+  const limitedRankingChanges = rankingChanges.slice(0, STORED_CHANGES_NUM)
+  await writeDBFile(NEWS_DB_NAME, limitedRankingChanges)
+
+  logSuccess('Ranking changes saved\n')
+}
