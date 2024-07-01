82f451c0a94ec7bb278dfd8b447b75a740b54eee
fentosv <fentos.dev@gmail.com>
2024-07-01 09:40:41 +0200

    Remove compareRankings from getAndSaveRankings

--- scraping/services/getAndSaveRankings.js
+++ scraping/services/getAndSaveRankings.js
@@ -1,4 +1,3 @@
-import { compareRankings } from './compareRankings.js'
 import { logSuccess } from '../utils/log.js'
 import { renameDBFile, writeDBFile } from '../utils/db.js'
 import scrapeRankings from '../scrapers/scrapeRankings.js'
@@ -11,8 +10,6 @@ export const getAndSaveRankings = async () => {
   const data = await scrapeRankings()
   await renameDBFile(RANKINGS_DB_NAME, RANKINGS_OLD_DB_NAME)
   await writeDBFile(RANKINGS_DB_NAME, data)
-
-  await compareRankings()
 
   const end = performance.now()
   const time = timeFormatter(end - start)
