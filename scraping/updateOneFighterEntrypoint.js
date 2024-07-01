69e3d8fb2530560901a276e5f74763a42c5d0049
fentosv <fentos.dev@gmail.com>
2024-07-01 10:15:58 +0200

    Move & rename updateOneFighterEntrypoint

--- scraping/updateOneFighterEntrypoint.js
+++ scraping/updateOneFighterEntrypoint.js
@@ -1,10 +1,10 @@
-import { logError, logSuccess } from '../utils/log.js'
-import { readDBFile } from '../utils/db.js'
-import scrapeFighter from '../scrapers/scrapeFighter.js'
-import timeFormatter from '../utils/timeFormatter.js'
-import { FIGHTERS_DB_NAME } from '../constants/names.js'
+import { logError, logSuccess } from './utils/log.js'
+import { readDBFile } from './utils/db.js'
+import scrapeFighter from './scrapers/scrapeFighter.js'
+import timeFormatter from './utils/timeFormatter.js'
+import { FIGHTERS_DB_NAME } from './constants/names.js'
 
-const run = async () => {
+const runUpdateOneFighterEntrypoint = async () => {
   const start = performance.now()
   const ARG_SELECTOR = '--'
   const firstArg = process.argv.find((el) => el.startsWith(ARG_SELECTOR))
@@ -26,7 +26,7 @@ const run = async () => {
   const end = performance.now()
   const time = timeFormatter(end - start)
 
-  logSuccess(`\nTask finished in ${time}`)
+  logSuccess(`Task finished in ${time}`)
 }
 
-run()
+runUpdateOneFighterEntrypoint()
