16a157b4a1a70c212f80f296e84d6e0eb7410517
fentosv <fentos.dev@gmail.com>
2024-07-02 14:19:46 +0200

    Improve rankingsEntrypoint log

--- scraping/rankingsEntrypoint.js
+++ scraping/rankingsEntrypoint.js
@@ -2,10 +2,8 @@ import { compareAndSaveRankings } from './services/compareAndSaveRankings.js'
 import { getAndSaveRankings } from './services/getAndSaveRankings.js'
 
 const runRankingsEntrypoint = async () => {
-  console.log('Starting scraping process from runRankingsEntrypoint...')
+  console.log('Starting "runRankingsEntrypoint"...\n')
   await getAndSaveRankings()
-
-  console.log('Starting compareAndSaveRankings...')
   await compareAndSaveRankings()
 }
 
