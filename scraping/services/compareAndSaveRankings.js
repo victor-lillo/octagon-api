e4dd2ea9aa23357fea22103414133ab04e07f506
fentosv <fentos.dev@gmail.com>
2024-07-02 14:04:11 +0200

    Reorder imports in compareAndSaveRankings

--- scraping/services/compareAndSaveRankings.js
+++ scraping/services/compareAndSaveRankings.js
@@ -1,9 +1,9 @@
 import { getRankingsChanges } from './getRankingsChanges.js'
 import { logSuccess } from '../utils/log.js'
+import { timeFormatter } from '../utils/timeFormatter.js'
 import { writeDBFile } from '../utils/db.js'
 import { NEWS_DB_NAME } from '../constants/names.js'
 import { STORED_CHANGES_NUM } from '../constants/config.js'
-import { timeFormatter } from '../utils/timeFormatter.js'
 
 export const compareAndSaveRankings = async () => {
   const start = performance.now()
