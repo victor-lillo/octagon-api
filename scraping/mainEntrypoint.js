5cb5f4e2bcce383f1f9f57a515974fcdc2e68bd8
fentosv <fentos.dev@gmail.com>
2024-07-02 13:35:41 +0200

    Update mainEntrypoint

--- scraping/mainEntrypoint.js
+++ scraping/mainEntrypoint.js
@@ -1,10 +1,16 @@
-import getAndSaveFighters from './services/getAndSaveFighters.js'
-import getRankings from './rankingsEntrypoint.js'
+import { compareAndSaveRankings } from './services/compareAndSaveRankings.js'
+import { getAndSaveFighters } from './services/getAndSaveFighters.js'
+import { getAndSaveRankings } from './services/getAndSaveRankings.js'
 import { logInfo, logSuccess } from './utils/log.js'
 
 const runMainEntrypoint = async () => {
   logInfo('RUNNING COMPLETE SCRAPER\n')
-  await getRankings()
+  console.log('Starting scraping process from runMainEntrypoint...')
+  await getAndSaveRankings()
+
+  console.log('Starting compareAndSaveRankings...')
+  await compareAndSaveRankings()
+
   await getAndSaveFighters()
   logSuccess('Scraper finished')
 }
