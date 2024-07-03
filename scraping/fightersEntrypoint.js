ee907626e8c41281e5ab8e820e65c0fc7778c514
fentosv <fentos.dev@gmail.com>
2024-07-03 10:54:03 +0200

    Remove log from fightersEntrypoint

--- scraping/fightersEntrypoint.js
+++ scraping/fightersEntrypoint.js
@@ -1,11 +1,9 @@
 import { getAndSaveFighters } from './services/getAndSaveFighters.js'
-import { logSuccess } from './utils/log.js'
 
 const runFightersEntrypoint = async () => {
   console.log('Starting "runFightersEntrypoint"...\n')
 
   await getAndSaveFighters()
-  logSuccess('Scraper finished')
 }
 
 runFightersEntrypoint()
