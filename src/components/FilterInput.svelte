a181c6bf37e85191176ea1292fb2e926c77741f5
fentosv <fentos.dev@gmail.com>
2024-07-14 15:40:12 +0200

    Add FilterInput.svelte component

---
+++ src/components/FilterInput.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let isChecked;
+  export let label;
+</script>
+
+<label>
+  {label}
+  {#if isChecked}
+    <slot name="ascIcon" />
+  {:else}
+    <slot name="descIcon" />
+  {/if}
+  <input class="hide" bind:checked={isChecked} type="checkbox" />
+</label>
+
+<style>
+  label {
+    gap: 0.5rem;
+    display: flex;
+  }
+
+  .hide {
+    display: none;
+  }
+</style>
