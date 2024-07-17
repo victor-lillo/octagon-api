3612c5de9ad599aebcf65f80d739e1fc57bee8dc
fentosv <fentos.dev@gmail.com>
2024-07-17 17:13:17 +0200

    Add arrayModifiers file

---
+++ src/utils/arrayModifiers.ts
@@ -0,0 +1,16 @@
+export function removeArrayElementByKey({
+  array,
+  keyToRemove,
+  keyValue,
+}: {
+  array: Array<{ [key: string]: string }>
+  keyToRemove: string
+  keyValue: string
+}) {
+  const index = array.findIndex((x) => x[keyToRemove] === keyValue)
+  const copy = array
+  if (index !== -1) {
+    copy.splice(index, 1)
+  }
+  return copy
+}
