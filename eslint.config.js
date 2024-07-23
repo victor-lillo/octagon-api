6a897510ebb94f16f41c550a41ee786958802626
fentosv <fentos.dev@gmail.com>
2024-07-23 09:24:51 +0200

    (Checkpoint) Add eslint.config.js post migration script

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,114 @@
+import globals from 'globals'
+import astroParser from 'astro-eslint-parser'
+import svelteParser from 'svelte-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 import eslintPluginAstro from 'eslint-plugin-astro'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
+  {
+    ignores: ['**/*.d.ts', 'dist/*'],
+  },
   ...eslintPluginAstro.configs.recommended,
   jsxA11y.flatConfigs.recommended,
   {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  {
+    files: ['**/*.astro'],
+
+    languageOptions: {
+      parser: astroParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+
     rules: {
       'astro/no-unused-css-selector': 'error',
       'astro/prefer-object-class-list': 'error',
+    },
+  },
+  ...compat
+    .extends('plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended')
+    .map((config) => ({
+      ...config,
+      files: ['**/*.svelte'],
+    })),
+  {
+    files: ['**/*.svelte'],
+
+    languageOptions: {
+      parser: svelteParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        parser: {
+          ts: '@typescript-eslint/parser',
+          js: 'espree',
+          typescript: '@typescript-eslint/parser',
+        },
+      },
+    },
+
+    rules: {
+      'svelte/no-target-blank': 'error',
+      'svelte/button-has-type': 'warn',
+
+      'svelte/html-self-closing': [
+        'error',
+        {
+          void: 'always',
+          component: 'always',
+          svelte: 'always',
+        },
+      ],
+    },
+  },
+  ...compat.extends('plugin:@typescript-eslint/recommended').map((config) => ({
+    ...config,
+    files: ['**/*.ts'],
+  })),
+  {
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/ban-tslint-comment': 'error',
+    },
+  },
+  {
+    files: ['**/*.astro/*.js', '*.astro/*.js'],
+
+    languageOptions: {
+      parser: tsParser,
     },
   },
 ]
