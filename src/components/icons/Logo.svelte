a83d81cad6e6e970ce455238594e1747a66c96cb
fentosv <fentos.dev@gmail.com>
2024-07-26 13:37:42 +0200

    Clean SVG code & move to /icons folder

--- src/components/icons/Logo.svelte
+++ src/components/icons/Logo.svelte
@@ -1,39 +1,43 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.66 101.08" class="logo"
-  ><g id="Capa_1" data-name="Capa 1"
-    ><polygon
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.66 101.08" class="logo">
+  <g>
+    <polygon
       points="95.02 15.18 59.66 0.54 24.3 15.19 9.66 50.54 24.31 85.9 59.66 100.54 95.02 85.89 109.66 50.54 95.02 15.18"
       fill="#1d1d1b"
       stroke="#1d1d1b"
       stroke-miterlimit="10"
-    /><polygon
+    />
+    <polygon
       points="92.61 17.59 59.66 3.94 26.71 17.59 13.06 50.54 26.71 83.49 59.66 97.14 92.61 83.49 106.26 50.54 92.61 17.59"
       fill="#1d1d1b"
       stroke="#fff"
       stroke-miterlimit="10"
       stroke-width="3"
-    /></g
-  ><g id="Capa_2" data-name="Capa 2"
-    ><text /><path
+    />
+  </g>
+  <g>
+    <path
       d="M33.45,71.79l.86-8.43H19.69l-5.34,8.43H1L32.11,26.63H48.32L46.74,71.79Zm2-18.4L36.16,46c.09-.8.21-1.94.36-3.43s.32-3,.52-4.62.39-2.89.55-3.92c-.55,1-1.28,2.29-2.16,3.8s-1.78,3-2.68,4.48S31.09,45,30.47,46l-4.63,7.38Z"
       fill="#fff"
       stroke="#1d1d1b"
       stroke-linejoin="round"
       stroke-width="2"
-    /><path
+    />
+    <path
       d="M79,26.81q8.55,0,11.46,3.73t.41,10.58a27.24,27.24,0,0,1-3,5.9,22.21,22.21,0,0,1-4.7,5,21.82,21.82,0,0,1-6.53,3.51A26.22,26.22,0,0,1,68,56.87H64.63L59.2,71.79H47.05l16.37-45Zm-3.85,9.82H72L68.23,47h2.4a9.4,9.4,0,0,0,3.12-.54,7.53,7.53,0,0,0,2.81-1.75,8.38,8.38,0,0,0,2-3.24,4,4,0,0,0,0-3.47C78,37.07,76.9,36.63,75.13,36.63Z"
       fill="#fff"
       stroke="#1d1d1b"
       stroke-linejoin="round"
       stroke-width="2"
-    /><path
+    />
+    <path
       d="M82.08,71.79l16.36-45h12.22l-16.37,45Z"
       fill="#fff"
       stroke="#1d1d1b"
       stroke-linejoin="round"
       stroke-width="2"
-    /></g
-  ></svg
->
+    />
+  </g>
+</svg>
 
 <style>
   .logo {
